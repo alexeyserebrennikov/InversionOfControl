f182806400249d0eec941c39c3c83c58cef77953
alexeyserebrennikov <71338379+alexeyserebrennikov@users.noreply.github.com>
2021-01-13 18:37:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InversionOfControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero hero = new Hero(new PVPArena());
+            hero.Arena();
+            Console.ReadLine();
+            hero.BattleArena = new PVEArena();
+            hero.Arena();
+            Console.ReadLine();
+        }
+         
+        interface IBattleArena
+        {
+            void Arena();
+        }
+
+        class Hero
+        {
+            public IBattleArena BattleArena { get; set; }
+
+            public Hero(IBattleArena battleArena)
+            {
+                this.BattleArena = battleArena;
+            }
+
+            public void Arena()
+            {
+                BattleArena.Arena();
+            }
+        }
+
+        class PVPArena : IBattleArena
+        {
+            public void Arena()
+            {
+                Console.WriteLine("Герой подключается к PVP арене");
+            }
+        }
+
+        class PVEArena : IBattleArena
+        {
+            public void Arena()
+            {
+                Console.WriteLine("Герой подключается к PVE арене");
+            }
+        }
+    }
+}
